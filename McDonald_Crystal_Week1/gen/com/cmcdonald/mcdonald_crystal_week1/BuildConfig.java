bb87ef144308ae2d689ba1ca38ae87ac38bf2b3e
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-02-05 09:44:52 -0800

    setting up folder
    Java 1 project example

---
+++ McDonald_Crystal_Week1/gen/com/cmcdonald/mcdonald_crystal_week1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cmcdonald.mcdonald_crystal_week1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
